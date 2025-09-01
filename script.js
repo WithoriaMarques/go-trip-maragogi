6f0b3542478be4672f1c663ab74f0f9910fcbb3f
WithoriaMarques <wmwithoria@gmail.com>
2025-08-31 23:16:17 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+let slides = document.querySelectorAll('.carousel-slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === index) {
+      slide.classList.add('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Troca a cada 4 segundos
+setInterval(nextSlide, 4000);
